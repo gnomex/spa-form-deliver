1b2dda9cfbadd412f3b63d95b2f39697c37dc048
Kenner Kliemann <kenner.hp@gmail.com>
2016-03-15 19:34:35 -0300

    More firulas

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,25 @@ var router = express.Router();
 var nodemailer = require('nodemailer');
 
 // create reusable transporter object using the default SMTP transport
-var transporter = nodemailer.createTransport('smtps://kenner.hp%40gmail.com:lamivbvfbqbsxtmp@smtp.gmail.com');
+var smtp = normalizePort(process.env.SMTPCREDENTIALS || '');
+var transporter = nodemailer.createTransport(smtp);
 
-var SECRET = "c517fecf7fe21b87e8f5f986f485da1f5b2d90103695a8c46628e72e883af04fc5d84c279e3d24435bf2c9c610d5600ec96d37e06b4a1242e71a1f04e8e36d86";
+var SECRET = process.env.SPASECRET || '';
 
 router.post('/deliverforme', function(req, res, next) {
-  if (req.body.key !== SECRET) { res.status(400).send("Impossível registrar"); }
+  if (req.body.key !== SECRET) { res.status(400).send(); }
 
   var data = req.body;
+  var sended_at = new Date(data.timestamp);
+  var received_at = Date.now();
+
+  var message = ["Nova mensagem através do site.\n\n", data['form[content]'], "\n\n", "Enviado em ", sended_at.toString(), ". Processado em ", received_at.toString()].join(' ');
 
   var mailOptions = {
     to: 'kenner.hp@gmail.com',
     from: [data['form[name]'],"<",data['form[email]'],">"].join(' '),
     subject: data['form[subject]'],
-    text: data['form[content]']
+    text: message;
   };
 
   transporter.sendMail(mailOptions, function(error, info){
