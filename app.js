340b9dc510d650c87a7e17c19ec1fb27c9e5fedd
Kenner Kliemann <kenner.hp@gmail.com>
2016-03-15 22:01:54 -0300

    lol

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var cors = require('cors');
-var CORSORIGIN = process.env.CORS || '*';
 var routes = require('./routes/index');
 
 var app = express();
@@ -22,13 +20,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-var corsOptions = {
-  origin: CORSORIGIN,
-  methods: 'POST'
-};
-
-app.use(cors(corsOptions));
 app.use('/', routes);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
+var cors = require('cors');
+var CORSORIGIN = process.env.CORS || '*';
+
+var corsOptions = {
+  origin: CORSORIGIN,
+  methods: 'POST'
+};
 
 // create reusable transporter object using the default SMTP transport
 var smtp = process.env.SMTPCREDENTIALS || '';
@@ -9,13 +16,17 @@ var transporter = nodemailer.createTransport(smtp);
 var SECRET = process.env.SPASECRET || '';
 var SENDTO = process.env.EMAILS || '';
 
-router.post('/deliverforme', function(req, res, next) {
+function validate_data(req, res, next) {
   var data = req.body;
+  if (data.key && data.timestamp && data['form[content]'] && data['form[name]'] && data['form[email]'] ) {
+    res.sendStatus(404);
+  } else {
+    next();
+  }
+}
 
-  if (data.key && data.timestamp && data['form[content]'] && data['form[name]'] && data['form[email]'] ) { res.sendStatus(404); }
-
-  if (data.key !== SECRET) { res.sendStatus(400); }
-
+function deliver_email(req, res, next) {
+  var data = req.body;
   var sended_at = new Date(parseFloat(data.timestamp));
   var received_at = new Date(Date.now());
 
@@ -35,9 +46,19 @@ router.post('/deliverforme', function(req, res, next) {
 
   transporter.sendMail(mailOptions, function(error, info){
     if(error) { res.sendStatus(403); }
-  });
 
-  res.sendStatus(201);
-});
+    res.sendStatus(201);
+  });
+}
+
+router.post('/deliverforme', cors(corsOptions), validate_data, function(req, res, next) {
+
+  if (req.body.key !== SECRET) {
+    res.sendStatus(400);
+  } else  {
+    next();
+  }
+
+}, deliver_email);
 
 module.exports = router;
